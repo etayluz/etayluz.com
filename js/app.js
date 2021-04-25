5c6d1f2f221e7b2535b9532937dbde3d542b8bc6
Etay Luz <etayluz@gmail.com>
2021-04-25 00:59:35 -0400

    Save work

--- js/app.js
+++ js/app.js
@@ -1,25 +1,4 @@
-$(document).ready(function(e) {
-	barHeight = 60
-	fontSize = 20
-	
-	// $("#homeFrame").append('<div id="homeFrame" style="position:absolute;top:0px;left:50px;width:0%;bottom:0px;background-color:#d7d8d7;"></div>')
-
-	textColor = '#000080'
-
-	$("#introhomeFrame").append('')
-	$("#introTitle").append('')
-
-	$("#bioFrame").append('<div id="bioTitle" style="position:absolute;top:20%;left:20%;width:60%;color:#393F44;font-size:45px;font-family:Georgia,sans-serif;line-height:1.3;text-align:center;background-color:Transparent;">BIO</div>')
-	$("#bioTitle").append('<div id="bioText" style="position:absolute;top:120%;left:7.5%;width:85%;color:#393F44;font-size:31px;font-family:Georgia,sans-serif;line-height:1.4;text-align:center;background-color:Transparent">Etay was born in 1984 in Tel Aviv, Israel but at age 9 his family moved to Chicago. In 2005 Etay graduated from the University of Illinois in Urbana-Champaign with a degree in Electrical Engineering. Since then he has worked for various technology corporations and startups in Tel Aviv, San Francisco, New York, Miami, and Chicago.</div>')
-
-	$("#bioFrame").append('<div id="bioFrame2" style="position:absolute;top:100%;left:0px;right:0px;height:120%;bottom:0px;background-color:#fae8bd"></div>')
-	
-	$("#bioFrame2").append('<div id="biohomeFrame" style="position:absolute;top:0px;left:0px;width:50%;bottom:0px"></div>')
-	$("#bioFrame2").append('<div id="biohomeFrame" style="position:absolute;top:0px;right:0px;width:50%;bottom:0px;background-color:clear;"></div>')
-	// $("#biohomeFrame").append('<img id="Etay2" src="./images/Etay3_trans.png" style="position:absolute;left:0px;top:0px;bottom:0px;right:0px;display:block;margin-left:auto;margin-right:auto;width:50%;height:100%;object-fit:cover;"/>')
-	// $("#biohomeFrame").append('<div id="introTitle" style="position:absolute;top:20%;left:10%;width:80%;color:#393F44;font-size:50px;font-family:Georgia,sans-serif;line-height:1.3;color:#e80fe1">WHAT IS THIS?</div>')
-	$("#biohomeFrame").append('<img id="Etay2" src="./images/baby.png" style="position:absolute;left:0px;top:0px;bottom:0px;right:0px;display:block;margin-left:auto;margin-right:auto;width:100%;height:100%;object-fit:contain;"/>')
-	$("#biohomeFrame").append('<div id="bioText" style="position:absolute;top:30%;left:110%;width:85%;color:#393F44;font-size:31px;font-family:Georgia,sans-serif;line-height:1.4;text-align:center;background-color:Transparent">Today Etay works for IBM as a software engineer implementing IBM\'s artifical intelligence and machine learning solutions for clients. Prior to joining IBM, Etay has worked for Motorola, Intel, Qualcomm, and a handful of startups.</div>')
+$(document).ready(function(e) {	
 
 	$("#bioFrame2").append('<div id="hobbiesFrame" style="position:absolute;top:100%;left:0px;right:0px;height:120%;bottom:0px;background-color:#d6d8d6"></div>')
 	$("#hobbiesFrame").append('<div id="hobbiesTitle" style="position:absolute;top:3%;left:19%;width:60%;color:#393F44;font-size:45px;font-family:Georgia,sans-serif;line-height:1.3;text-align:center;background-color:Transparent;">HOBBIES</div>')
