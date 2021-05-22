4755a9f3fb9b4872bb488d7d79497e8dcef9efa4
Etay Luz <etayluz@gmail.com>
2021-05-22 17:23:22 -0400

    Save work

--- js/app.js
+++ js/app.js
@@ -52,7 +52,7 @@ $(document).ready(function(e) {
 	$("#bioFrame2").append('<div id="hobbiesFrame" style="position:absolute;top:100%;left:0px;right:0px;height:120%;bottom:0px;background-color:#d6d8d6"></div>')
 	$("#hobbiesFrame").append('<div id="hobbiesTitle" style="position:absolute;top:3%;left:19%;width:60%;color:#393F44;font-size:45px;font-family:brandon-grot-w01-light,sans-serif;line-height:1.3;text-align:center;background-color:Transparent;">HOBBIES</div>')
 	$("#hobbiesFrame").append('<div id="hobbieshomeFrame" style="position:absolute;top:15%;right:0px;width:50%;bottom:0px;background-color:clear;"></div>')
-	$("#hobbieshomeFrame").append('<img id="Etay2" src="./images/PaddleBoard.JPG" style="position:absolute;left:0px;top:0px;bottom:0px;right:0px;display:block;margin-left:auto;margin-right:auto;width:100%;height:100%;object-fit:contain;"/>')
+$("#hobbieshomeFrame").append('<img id="Etay2" src="./images/PaddleBoard.JPG" style="position:absolute;left:0px;top:0px;bottom:0px;right:0px;display:block;margin-left:auto;margin-right:auto;width:100%;height:100%;object-fit:contain;"/>')
 	// $("#hobbieshomeFrame").append('<div id="hobbiesTitle" style="position:absolute;top:30%;left:10%;width:80%;color:#393F44;font-size:50px;font-family:brandon-grot-w01-light,sans-serif;line-height:1.3;color:#fae8bd">Hobbies</div>')
 	$("#hobbieshomeFrame").append('<div id="text" style="position:absolute;top:30%;left:10%;width:80%;color:#393F44;font-size:34px;font-family:brandon-grot-w01-light,sans-serif;line-height:1.3;color:#393F44;line-height:1.5;">Hobbies include playing/singing on my guitar, hiking, kayaking, canoing, beach time, and working on this website</div>')
 	
